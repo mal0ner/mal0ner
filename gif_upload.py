534b45fbe47fb08da52e759f269bdc6fa0d87057
Cameron Malone <cameronmkm2@gmail.com>
2024-01-02 18:18:51 +1100

    did i do something wrong?

--- gif_upload.py
+++ gif_upload.py
@@ -1,24 +1,19 @@
 import gifos
 
 
-def main():
-    gif = gifos.utils.upload_imgbb("no-loop.gif", 129600)
-    readme_file_content = rf"""
+image = gifos.utils.upload_imgbb("no-loop.gif", 129600)  # 1.5 days expiration
+readme_file_content = rf"""
 <div align="justify">
 <picture>
-    <source media="(prefers-color-scheme: dark)" srcset="{gif.url}">
-    <source media="(prefers-color-scheme: light)" srcset="{gif.url}">
-    <img alt="GIFOS_README_TERMINAL" src="{gif.url}">
+    <source media="(prefers-color-scheme: dark)" srcset="{image.url}">
+    <source media="(prefers-color-scheme: light)" srcset="{image.url}">
+    <img alt="GIFOS_README_TERMINAL" src="{image.url}">
 </picture>
 
 <sub><i>Generated automatically using [x0rzavi/github-readme-terminal](https://github.com/x0rzavi/github-readme-terminal)</i></sub>
 
 </div>
     """
-    with open("README.md", "w") as fp:
-        fp.write(readme_file_content)
-        print("INFO: README.md file generated")
-
-
-if __name__ == "__main__":
-    main()
+with open("README.md", "w") as fp:
+    fp.write(readme_file_content)
+    print("INFO: README.md file generated")
